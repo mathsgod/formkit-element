26774400f883f24fd9ab4328e0a493a077c90d14
Raymond Chong <mathsgod@yahoo.com>
2022-11-15 11:00:52 +0800

    readme

--- src/pages/rate.vue
+++ src/pages/rate.vue
@@ -2,20 +2,24 @@
 import { ref } from "vue";
 const data = ref({
     rate: 3,
+    rate_half: 3.5,
 })
 
 
 </script>
 
 <template>
-
-    <FormKit type="elForm" v-model="data">
-        <FormKit type="elRate" label="elRate" name="rate" help="help text" />
+    <FormKit type="form" v-model="data">
         <FormKit type="elFormRate" label="elFormRate" name="rate" />
+
+        <FormKit type="elRate" label="elRate" name="rate" help="help text" />
+
+        <FormKit type="elRate" label="elRate" name="rate" :texts="['oops', 'disappointed', 'normal', 'good', 'great']"
+            show-text />
+
+        <FormKit type="elRate" label="elRate (allow-half)" name="rate_half" allow-half />
     </FormKit>
 
-
     <pre wrap>{{ data }}</pre>
-
 
 </template>
