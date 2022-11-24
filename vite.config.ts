a0b69fe56822bf83ae196376be3e309ec0c7b31e
Raymond Chong <mathsgod@yahoo.com>
2022-11-24 10:24:50 +0800

    build without element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue',"@formkit/vue"],
+      external: ['vue',"@formkit/vue","element-plus"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
